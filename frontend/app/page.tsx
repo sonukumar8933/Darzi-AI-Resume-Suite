43b733ffe5cedee97d8a3ebd329aaa3e7ea437d1
Manthan Awgan <manthanawgan@gmail.com>
2025-08-03 00:45:06 +0530

    made landing page but still have to work on some minore changes

---
+++ frontend/app/page.tsx
@@ -0,0 +1,11 @@
+import HeroHeader from '../components/header';
+import HeroSection from '../components/hero-section';
+
+export default function Home() {
+  return (
+    <div>
+      <HeroHeader />
+      <HeroSection />
+    </div>
+  );
+}
--- src/task-1/README.md
+++
@@ -1,7 +0,0 @@
-<p align="center">
-  <img width="30%" height="30%" src="https://github.com/user-attachments/assets/fcfbc33d-970e-42b0-befa-690e172235a8">
-</p>
-
-# Darzi: AI Resume Suite
-
-## Task 1
