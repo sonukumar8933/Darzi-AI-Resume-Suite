13ee256e40571d55ada2d2ae95e877892a76ca00
Swayam <swayam.panda200@gmail.com>
2025-08-06 12:50:56 +0530

    Updated dashboard links

--- frontend/components/hero-section.tsx
+++ frontend/components/hero-section.tsx
@@ -154,7 +154,7 @@ export default function HeroSection() {
                                                     asChild
                                                     size="lg"
                                                     className="rounded-xl px-5 text-base">
-                                                    <Link href="#link">
+                                                    <Link href="/Dashboard">
                                                         <span className="text-nowrap">Dashboard</span>
                                                     </Link>
                                                 </Button>
